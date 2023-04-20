0b0972396c446aeab38d99cb38cf2e8a61426ace
Gustavo Henrique Pereira Junqueira <102169187+GustaHenriPe@users.noreply.github.com>
2023-04-20 14:01:26 -0300

    ex criado

---
+++ ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+/*while(c <= 1){
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+/*do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 0)*/
+for(var c=1; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}
+
