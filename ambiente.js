175a51ae2332142ce5d0427297f7b66f8ced70c7
Gustavo Henrique Pereira Junqueira <102169187+GustaHenriPe@users.noreply.github.com>
2023-04-24 16:41:14 -0300

    exs criados

--- ambiente.js
+++ ambiente.js
@@ -1,14 +1,79 @@
-var c = 1
-/*while(c <= 1){
-    console.log(`Passo ${c}`)
-    c++
-}*/
 
-/*do{
+/*var c = 1
+
+while(c <= 1){
     console.log(`Passo ${c}`)
     c++
-}while(c <= 0)*/
+}
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 0)
+
 for(var c=1; c <= 10; c++){
     console.log(`Passo ${c}`)
 }
 
+let num = [5, 8, 2, 9, 3]
+num[3] = 6
+num.push(7)
+num.sort()
+num.length
+num.indexOf(7)
+console.log(`Nosso vetor é o ${num}`)
+
+for (var pos = 0; pos < num.length; pos++){
+    console.log(num[pos])
+}
+
+for(let pos in num){
+    console.log(num[pos])
+}
+
+function parimpar(n){
+    if (n%2 == 0){
+        return 'par'
+    }else {
+        return 'impar'
+    }
+}
+
+let res = parimpar(542)
+
+console.log(res)
+console.log(parimpar(55))
+
+function soma (n1=0, n2=0){
+    return n1 + n2
+}
+
+console.log(soma(1))
+
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(5))
+ 
+
+function fatorial (n){
+    let fat = 1
+    for(let c = n; c > 1; c--){
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
+
+function fatorial(n){
+    if (n == 1){
+        return 1
+    }else{
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(5))
+*/
